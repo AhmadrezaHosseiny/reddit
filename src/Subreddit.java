6c94dbc7dce61d5c078e620ad260e45c81ca8e89
AhmadrezaHosseiny <a.r.hosseiny1384@gmail.com>
2024-04-04 10:31:40 +0330

    created

--- src/Subreddit.java
+++ src/Subreddit.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
 public class Subreddit {
+    public String topic;
+    public ArrayList<Account> memberAccounts = new ArrayList<>();
+    public static ArrayList<Post> posts = new ArrayList<>();
+    public Account admin;
+    public void showPosts() {
+        for (Post p : posts) {
+            //p.showPostText();
+        }
+    }
+    public void show() {
+        //todo println topic admin
+    }
+
+
+
+
+
+
+
+
+
 }
