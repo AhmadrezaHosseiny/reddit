545458f421bb8cb7fd360d8e9e77ef36e50e5fe2
AhmadrezaHosseiny <a.r.hosseiny1384@gmail.com>
2024-04-04 10:30:06 +0330

    created

--- src/Comment.java
+++ src/Comment.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
 public class Comment {
+    public String text;
+    public Account account;
+    public ArrayList<Karma> karmas = new ArrayList<>();
+    public void show() {
+        //todo : account.ussername , text ,karma.sum(karma)
+    }
+
+
+
+
+
+
+
+
 }
